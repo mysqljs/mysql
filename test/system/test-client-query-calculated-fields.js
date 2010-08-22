9b2d363ba9384aa0d867f75be79a34e48fa722b7
Alan Gutierrez <alan@blogometer.com>
2010-08-22 20:19:40 +0200

    Field packet properties can be optional
    This patch fixes issue #5, see:
    
    http://github.com/felixge/node-mysql/issues#issue/5
    
    Original patch: http://github.com/bigeasy/node-mysql/commit/fd31a70bbf1d29f87b5b81d4433ecb8f9a24fe84
    
    Signed-off-by: Felix Geisendörfer <felix@debuggable.com>

---
+++ test/system/test-client-query-calculated-fields.js
@@ -0,0 +1,16 @@
+require('../common');
+var Client = require('mysql').Client,
+    client = Client(TEST_CONFIG),
+    gently = new Gently();
+
+// our test db might not exist yet, so don't try to connect to it
+client.database = '';
+client.connect();
+
+client.query('SELECT 1 as field_a, 2 as field_b', function(err, results) {
+  if (err) throw err;
+
+  console.log(results);
+  assert.equal(results[0].field_a, 1);
+  assert.equal(results[0].field_b, 2);
+});
