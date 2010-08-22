1f36260d4ba8c698944f1b6bdf9349a818631c2a
Felix Geisendörfer <felix@debuggable.com>
2010-08-22 20:49:26 +0200

    Add failing sequential query test

---
+++ test/system/test-client-sequential-query.js
@@ -0,0 +1,24 @@
+require('../common');
+var Client = require('mysql').Client,
+    client = Client(TEST_CONFIG),
+    gently = new Gently();
+
+// our test db might not exist yet, so don't try to connect to it
+client.database = '';
+client.connect();
+
+client.debug = true;
+
+client.query('SELECT 1 as field_a, 2 as field_b', function(err, results) {
+  if (err) throw err;
+
+  assert.equal(results[0].field_a, 1);
+  assert.equal(results[0].field_b, 2);
+});
+
+client.query('SELECT 3 as field_c', function(err, results) {
+  if (err) throw err;
+
+  assert.equal(results[0].field_c, 3);
+  client.end();
+});
