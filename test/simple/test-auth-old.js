4cc30d31fbaac40d9d41a40bf1a542b9dbacfadb
Cal Henderson <cal@iamcal.com>
2010-09-01 16:02:21 -0500

    let's see which one is failing

--- test/simple/test-auth-old.js
+++ test/simple/test-auth-old.js
@@ -1,11 +1,15 @@
 require('../common');
+var sys = require('sys');
 
 var auth = require('mysql/auth');
 
 function test_hash_password(password, bytes){
 	var expected = new Buffer(bytes);
 	var actual = auth.hashPassword(password);
+
+	sys.print('hashPassword('+password+')...');
 	assert.deepEqual(actual, expected); // , 'hashPassword('+password+') failed');
+	sys.print('ok\n');
 }
 
 test_hash_password('root',			[0x67, 0x45, 0x7E, 0x22, 0x6a, 0x1a, 0x15, 0xbd]);
