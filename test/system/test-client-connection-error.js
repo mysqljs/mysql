2599dce7ee8cfc8f4230006d03af91d686cc7e70
frankgrimm@gmail.com <frankgrimm@gmail.com>
2010-10-25 16:51:05 +0200

    Add ENOTFOUND handling to connect function and test.

--- test/system/test-client-connection-error.js
+++ test/system/test-client-connection-error.js
@@ -3,9 +3,10 @@ var Client = require('mysql').Client,
     client = Client(TEST_CONFIG),
     gently = new Gently(),
     ECONNREFUSED = process.binding('net').ECONNREFUSED;
+    ENOTFOUND = process.binding('net').ENOTFOUND;
 
 client.host = 'BADHOST';
 
 client.connect(gently.expect(function connectCb(err, result) {
-  assert.equal(err.errno, ECONNREFUSED);
+  assert.ok(err.errno & (ECONNREFUSED | ENOTFOUND));
 }));
