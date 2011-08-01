9fa216a82d322d9980e0d81b11a89353d105ffff
Felix Geisendörfer <felix@debuggable.com>
2011-08-01 13:39:39 +0200

    Fix first system test
    Move error constants into own module

--- test/system/test-client-bad-credentials.js
+++ test/system/test-client-bad-credentials.js
@@ -1,10 +1,10 @@
-require('../common');
-var Client = require('mysql').Client,
-    client = Client(TEST_CONFIG),
-    gently = new Gently();
+var common = require('../common');
+var mysql = require(common.dir.lib + '/mysql');
+var client = mysql.createClient(TEST_CONFIG);
+var gently = new Gently();
 
 client.password = 'WRONG PASSWORD';
 
 client.connect(gently.expect(function connectCb(err, result) {
-  assert.equal(err.number, Client.ERROR_ACCESS_DENIED_ERROR);
+  assert.equal(err.number, mysql.ERROR_ACCESS_DENIED_ERROR);
 }));
