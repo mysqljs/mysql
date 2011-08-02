b2eb0d5645fd8a87e6893c512b482ae726e020cc
Felix Geisendörfer <felix@debuggable.com>
2011-08-02 15:10:14 +0200

    Port more query tests

--- test/slow/test-client-query.js
+++ test/slow/test-client-query.js
@@ -17,3 +17,49 @@ test('Virtual fields resulting from an operation', function(done) {
     done(err);
   });
 });
+
+test('Selecting an empty string', function(done) {
+  this.client.query('SELECT "" as field_a', function(err, results) {
+    assert.strictEqual(results[0].field_a, "");
+    done(err);
+  });
+});
+
+test('Long fields', function(done) {
+  function makeString(length) {
+    var str = '';
+    for (var i = 0; i < length; i++) {
+      str += 'x';
+    }
+    return str;
+  }
+
+  var field_a = makeString(250),
+      field_b = makeString(251),
+      field_c = makeString(512),
+      field_d = makeString(65537);
+
+  var self = this;
+  function test(last) {
+    var query = self.client.query(
+      'SELECT ? as field_a, ? as field_b, ? as field_c, ? as field_d',
+      [field_a, field_b, field_c, field_d],
+      function(err, results) {
+      if (err) throw err;
+
+      assert.equal(results[0].field_a, field_a);
+      assert.equal(results[0].field_b, field_b);
+      assert.equal(results[0].field_c, field_c);
+      assert.equal(results[0].field_d, field_d);
+
+      if (last) {
+        done(err);
+      }
+    });
+  }
+
+  // We execute this test twice to be sure the parser is in a good state after
+  // each run.
+  test();
+  test(true);
+});
