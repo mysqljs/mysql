852da62ee90c296cc6f74bc89114e76158eca1e3
Felix Geisendörfer <felix@debuggable.com>
2011-08-22 15:54:34 +0200

    Fix #96

--- test/fast/test-client.js
+++ test/fast/test-client.js
@@ -3,14 +3,24 @@ var assert = require('assert');
 var test = common.fastOrSlow.fast();
 var Client = require(common.dir.lib + '/client');
 
+var client;
 test.before(function() {
-  this.client = new Client();
+  client = new Client();
 });
 
 test('#format() does not manipulate params parameter', function() {
   var sql = '?';
   var params = [1];
 
-  this.client.format(sql, params);
+  client.format(sql, params);
   assert.equal(params.length, 1);
+});
+
+// https://github.com/felixge/node-mysql/issues/96
+test('Timeout reconnect works with empty queue', function() {
+  // A non-error packet
+  var packet = {};
+
+  // This must not throw an error
+  client._handlePacket(packet);
 });
