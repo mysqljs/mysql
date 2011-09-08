4a9a0a8d69eef2159632418d931ad63ebca47fc1
Felix Geisendörfer <felix@debuggable.com>
2011-09-08 08:27:51 -0400

    Simplify test case

--- test/slow/test-client-connect.js
+++ test/slow/test-client-connect.js
@@ -2,13 +2,14 @@ var common = require('../common');
 var assert = require('assert');
 var test = common.fastOrSlow.slow();
 
+var client;
 test.before(function() {
-  this.client = common.createClient();
+  client = common.createClient();
 });
 
 test.after(function(done) {
-  var connected = this.client.connected;
-  this.client.end(function(err) {
+  var connected = client.connected;
+  client.end(function(err) {
     (connected)
       ? assert.equal(err, null)
       : assert.notEqual(err, null);
@@ -18,7 +19,7 @@ test.after(function(done) {
 });
 
 test('Client tries to connect automatically', function(done) {
-  this.client.query('SELECT 1', function(err, results) {
+  client.query('SELECT 1', function(err, results) {
     assert.deepEqual(results, [{1: 1}]);
     done(err);
   });
@@ -26,15 +27,15 @@ test('Client tries to connect automatically', function(done) {
 
 test('Connection errors are delegated to callback', function(done) {
   // Port number outside of range -> triggers connection error
-  this.client.port = 999999999;
+  client.port = 999999999;
 
   var callbacks = [];
-  this.client.query('SELECT 1', function(err) {
+  client.query('SELECT 1', function(err) {
     assert.ok(err);
     callbacks.push(1);
   });
 
-  this.client.query('SELECT 2', function(err) {
+  client.query('SELECT 2', function(err) {
     callbacks.push(2);
     assert.ok(err);
 
@@ -44,15 +45,15 @@ test('Connection errors are delegated to callback', function(done) {
 });
 
 test('Bad credentials', function(done) {
-  this.client.password = 'thispassworddoesnotreallywork';
+  client.password = 'thispassworddoesnotreallywork';
 
   var callbacks = [];
-  this.client.query('SELECT 1', function(err) {
+  client.query('SELECT 1', function(err) {
     assert.ok(err);
     callbacks.push(1);
   });
 
-  this.client.query('SELECT 2', function(err) {
+  client.query('SELECT 2', function(err) {
     assert.ok(err);
     callbacks.push(2);
 
@@ -63,15 +64,15 @@ test('Bad credentials', function(done) {
 
 test('Reconnecting a closed client works', function(done) {
   var cbs = [];
-  this.client.query('SELECT 1', function() {
+  client.query('SELECT 1', function() {
     cbs.push(1);
   });
 
-  this.client.end(function() {
+  client.end(function() {
     cbs.push(2);
   });
 
-  this.client.query('SELECT 1', function(err) {
+  client.query('SELECT 1', function(err) {
     assert.deepEqual(cbs, [1, 2]);
     done(err);
   });
@@ -79,15 +80,14 @@ test('Reconnecting a closed client works', function(done) {
 
 test('Reconnect on timeout', {timeout: 2000}, function(done) {
   // Not sure if we need all 3 of these, but they do the trick
-  this.client.query('SET interactive_timeout = 1');
-  this.client.query('SET wait_timeout = 1');
-  this.client.query('SET net_read_timeout = 1');
+  client.query('SET interactive_timeout = 1');
+  client.query('SET wait_timeout = 1');
+  client.query('SET net_read_timeout = 1');
 
-  var self = this;
-  this.client._socket.on('end', function() {
-    assert.equal(self.client.connected, false);
+  client._socket.on('end', function() {
+    assert.equal(client.connected, false);
 
-    self.client.query('SELECT 1', function(err) {
+    client.query('SELECT 1', function(err) {
       done(err);
     });
   });
