1fc9146eeae98147c4d63ec21eceb426320bc464
Felix Geisendörfer <felix@debuggable.com>
2011-11-26 09:51:42 +0100

    Inlcude root package instead

--- test/unit/test-mysql.js
+++ test/unit/test-mysql.js
@@ -1,7 +1,7 @@
 var common = require('../common');
 var assert = require('assert');
 var test   = require('utest');
-var mysql  = require(common.dir.lib + '/mysql');
+var mysql  = require(common.dir.root);
 
 test('mysql module', {
   'Package JSON is exported': function() {
