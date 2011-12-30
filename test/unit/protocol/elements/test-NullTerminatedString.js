6feb914754ede4b88ec14f8efca25572903a8ff6
Felix Geisendörfer <felix@debuggable.com>
2011-12-30 17:18:12 +0100

    Fix NullTerminatedString implementation

--- test/unit/protocol/elements/test-NullTerminatedString.js
+++ test/unit/protocol/elements/test-NullTerminatedString.js
@@ -23,9 +23,28 @@ test('NullTerminatedString', {
     assert.deepEqual(buffer, new Buffer([97, 98, 0x00]));
   },
 
+  'empty string': function() {
+    var string = new NullTerminatedString('');
+    var buffer = new Buffer([255]);
+
+    string.copy(buffer, 0);
+
+    assert.deepEqual(buffer, new Buffer([0]));
+  },
+
+
+  'length': function() {
+    var string = new NullTerminatedString('ab');
+
+    // a (1) + b (1) + 0x00 (1) = 3
+    assert.equal(string.length, 3);
+  },
+
   'multibyte length': function() {
     var string = new NullTerminatedString('Öl');
-    assert.equal(string.length, 3);
+
+    // Ö (2) + l (1) + 0x00 (1) = 4
+    assert.equal(string.length, 4);
   },
 });
 
