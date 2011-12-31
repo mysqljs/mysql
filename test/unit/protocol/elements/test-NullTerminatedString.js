f142017c9301c1c112d3013ad9e6d197c0895052
Felix Geisendörfer <felix@debuggable.com>
2011-12-31 08:33:51 +0100

    Implement NullTerminatedString#write

--- test/unit/protocol/elements/test-NullTerminatedString.js
+++ test/unit/protocol/elements/test-NullTerminatedString.js
@@ -46,5 +46,43 @@ test('NullTerminatedString', {
     // Ö (2) + l (1) + 0x00 (1) = 4
     assert.equal(string.length, 4);
   },
+
+  'write "ab" with one write': function() {
+    var string = new NullTerminatedString();
+    var ab     = new Buffer([97, 98, 0x00]);
+
+    var full = string.write(ab, 0, ab.length);
+
+    assert.ok(full);
+    assert.equal(string.value, 'ab');
+    assert.equal(string.length, 3);
+  },
+
+  'write "ab" with three writes': function() {
+    var string = new NullTerminatedString();
+    var ab     = new Buffer([97, 98, 0x00]);
+
+    var full = string.write(ab, 0, 1);
+    assert.ok(!full);
+
+    var full = string.write(ab, 1, 2);
+    assert.ok(!full);
+
+    var full = string.write(ab, 2, 3);
+    assert.ok(full);
+
+    assert.equal(string.value, 'ab');
+    assert.equal(string.length, 3);
+  },
+
+  'write empty string': function() {
+    var string = new NullTerminatedString();
+    var empty  = new Buffer([0x00]);
+
+    var full = string.write(empty, 0, 1);
+
+    assert.equal(string.value, '');
+    assert.equal(string.length, 1);
+  },
 });
 
