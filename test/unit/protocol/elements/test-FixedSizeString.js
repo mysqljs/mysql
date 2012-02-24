0d28b025a0a12a2500ad5948c4ee9b8417b110cf
Felix Geisendörfer <felix@debuggable.com>
2012-02-24 07:56:31 +0100

    New parse interface for FixedSizeString

--- test/unit/protocol/elements/test-FixedSizeString.js
+++ test/unit/protocol/elements/test-FixedSizeString.js
@@ -13,56 +13,53 @@ test('FixedSizeString', {
 
   'parse 1 bytes': function() {
     var string = new FixedSizeString(1);
-    var full = string.parse(new Buffer([1]), 0, 1);
+    var start = string.parse(new Buffer([1]), 0, 1);
 
-    assert.ok(full);
+    assert.equal(string.isDone(), true);
     assert.equal(string.bytesWritten, 1);
     assert.deepEqual(string.value, new Buffer([1]));
+    assert.equal(start, 1);
   },
 
   'parse 3 bytes': function() {
     var string = new FixedSizeString(3);
-    var full = string.parse(new Buffer([1, 2, 3]), 0, 3);
+    var start = string.parse(new Buffer([1, 2, 3]), 0, 3);
 
-    assert.ok(full);
+    assert.equal(string.isDone(), true);
     assert.equal(string.bytesWritten, 3);
     assert.deepEqual(string.value, new Buffer([1, 2, 3]));
+    assert.equal(start, 3);
   },
 
   'parse 3 bytes from bigger buffer': function() {
     var string = new FixedSizeString(3);
-    var full = string.parse(new Buffer([1, 2, 3, 4]), 0, 4);
+    var start = string.parse(new Buffer([1, 2, 3, 4]), 0, 4);
 
-    assert.ok(full);
+    assert.equal(string.isDone(), true);
     assert.equal(string.bytesWritten, 3);
     assert.deepEqual(string.value, new Buffer([1, 2, 3]));
+    assert.equal(start, 3);
   },
 
   'parse 3 bytes individually': function() {
     var string = new FixedSizeString(3);
 
-    var full = string.parse(new Buffer([1]), 0, 1);
-    assert.equal(full, false);
+    var start = string.parse(new Buffer([1]), 0, 1);
+    assert.equal(string.isDone(), false);
     assert.equal(string.bytesWritten, 1);
+    assert.equal(start, 1);
 
-    var full = string.parse(new Buffer([2]), 0, 1);
-    assert.equal(full, false);
+    var start = string.parse(new Buffer([2]), 0, 1);
+    assert.equal(string.isDone(), false);
     assert.equal(string.bytesWritten, 2);
+    assert.equal(start, 1);
 
-    var full = string.parse(new Buffer([3]), 0, 1);
-    assert.equal(full, true);
+    var start = string.parse(new Buffer([3]), 0, 1);
+    assert.equal(string.isDone(), true);
     assert.equal(string.bytesWritten, 3);
+    assert.equal(start, 1);
 
     assert.deepEqual(string.value, new Buffer([1, 2, 3]));
-  },
-
-  'setLength after instaniation': function() {
-    var string = new FixedSizeString();
-    string.setLength(5);
-
-    assert.ok(Buffer.isBuffer(string.value));
-    assert.equal(string.value.length, 5);
-    assert.equal(string.length, 5);
   },
 });
 
