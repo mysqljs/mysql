eb84c7d42e0b576940118dcf99d40564fb84f472
Felix Geisendörfer <felix@debuggable.com>
2012-03-13 16:28:38 +0100

    LengthCodedString parsing

--- test/unit/protocol/elements/test-LengthCodedString.js
+++ test/unit/protocol/elements/test-LengthCodedString.js
@@ -21,4 +21,49 @@ test('LengthCodedString', {
 
     assert.deepEqual(buffer, new Buffer([255, 0x03, 0xc3, 0x96, 0x6c, 255]));
   },
+
+  'parse: utf-8 string with offset': function() {
+    var string = new LengthCodedString('utf-8');
+    var buffer = new Buffer([255, 0x03, 0xc3, 0x96, 0x6c, 255]);
+
+    var offset = string.parse(buffer, 1, 5);
+    assert.equal(string.value, 'Öl');
+    assert.equal(offset, 5);
+    assert.equal(string.bytesWritten, 4);
+    assert.equal(string.isDone(), true);
+  },
+
+  'parse: utf-8 string (byte by byte)': function() {
+    var string = new LengthCodedString('utf-8');
+    var buffer = new Buffer([0x03, 0xc3, 0x96, 0x6c]);
+
+    var offset = string.parse(buffer, 0, 1);
+    assert.equal(offset, 1);
+    assert.equal(string.isDone(), false);
+
+    var offset = string.parse(buffer, 1, 2);
+    assert.equal(offset, 2);
+    assert.equal(string.isDone(), false);
+
+    var offset = string.parse(buffer, 2, 3);
+    assert.equal(offset, 3);
+    assert.equal(string.isDone(), false);
+
+    var offset = string.parse(buffer, 3, 4);
+    assert.equal(offset, 4);
+    assert.equal(string.value, 'Öl');
+    assert.equal(string.bytesWritten, 4);
+    assert.equal(string.isDone(), true);
+  },
+
+  'parse: empty string': function() {
+    var string = new LengthCodedString('utf-8');
+    var buffer = new Buffer([0x00]);
+
+    var offset = string.parse(buffer, 0, 1);
+    assert.equal(offset, 1);
+    assert.equal(string.isDone(), true);
+    assert.equal(string.value, '');
+    assert.equal(string.length, 1);
+  },
 });
