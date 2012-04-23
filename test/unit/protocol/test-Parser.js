c18f68ce833ffa45a1df1e2a7f8b7f9f24195d77
Felix Geisendörfer <felix@debuggable.com>
2012-04-23 16:37:19 +0200

    Some more tests
    Still not decided how I want to continue with this, so will leave it at
    this for the moment.

--- test/unit/protocol/test-Parser.js
+++ test/unit/protocol/test-Parser.js
@@ -3,7 +3,7 @@ var test   = require('utest');
 var assert = require('assert');
 var Parser = require(common.lib + '/protocol/Parser');
 
-function from(bytes) {
+function packet(bytes) {
   var buffer = new Buffer(bytes);
   var parser = new Parser();
 
@@ -14,12 +14,18 @@ function from(bytes) {
 
 test('Parser', {
   'parseUnsignedNumber: 1 byte': function() {
-    var value = from([5]).parseUnsignedNumber(1);
+    var value = packet([5]).parseUnsignedNumber(1);
     assert.equal(value, 5);
   },
 
   'parseUnsignedNumber: 2 bytes': function() {
-    var value = from([1, 1]).parseUnsignedNumber(2);
+    var value = packet([1, 1]).parseUnsignedNumber(2);
     assert.equal(value, 256 + 1);
+  },
+
+  'parseUnsignedNumber: honors offsets': function() {
+    var parser = packet([1, 2]);
+    assert.equal(parser.parseUnsignedNumber(1), 1);
+    assert.equal(parser.parseUnsignedNumber(1), 2);
   },
 });
