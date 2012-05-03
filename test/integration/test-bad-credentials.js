9406edf81b79e6ff114a41f7369c50209eb34386
Felix Geisendörfer <felix@debuggable.com>
2012-05-03 17:02:35 +0200

    Added test for bad credentials
    Also added support for err.code and generating the neccessary lookup
    file.

---
+++ test/integration/test-bad-credentials.js
@@ -0,0 +1,14 @@
+var common     = require('../common');
+var connection = common.createConnection({password: 'INVALID PASSWORD'});
+var assert     = require('assert');
+
+var err;
+connection.connect(function(_err) {
+  err = _err;
+});
+
+process.on('exit', function() {
+  assert.ok(/access denied/i.test(err.message));
+  assert.equal(err.code, 'ER_ACCESS_DENIED_ERROR');
+});
+
