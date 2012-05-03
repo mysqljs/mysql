147036e167eb39b5c1c34d7d76c62b1a7edae1a5
Felix Geisendörfer <felix@debuggable.com>
2012-05-03 16:21:54 +0200

    Make common.createConnection configurable

--- test/common.js
+++ test/common.js
@@ -1,15 +1,16 @@
 var common = exports;
 var path   = require('path');
+var _      = require('underscore');
 
 common.lib = path.join(__dirname, '../lib');
 
 var Mysql = require('../');
 
-common.createConnection = function() {
-  return Mysql.createConnection({
+common.createConnection = function(config) {
+  return Mysql.createConnection(_.extend({
     host     : process.env.MYSQL_HOST,
     port     : process.env.MYSQL_PORT,
     user     : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
-  });
+  }, config));
 };
