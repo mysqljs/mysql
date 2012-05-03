47379b4cda1103fc36cd4b66c6de17c6a3dd4427
Felix Geisendörfer <felix@debuggable.com>
2012-05-03 11:09:16 +0200

    Protocol queue, return of sequences

--- test/integration/test-simple-query.js
+++ test/integration/test-simple-query.js
@@ -1,19 +1,19 @@
 var common     = require('../common');
 var connection = common.createConnection();
 var assert     = require('assert');
-var rows       = undefined;
 
 connection.connect(function(err) {
   if (err) throw err;
-
-  connection.query('SELECT 1', function(err, _rows) {
-    if (err) throw err;
-
-    rows = _rows;
-
-    connection.end();
-  });
 });
+
+var rows = undefined;
+connection.query('SELECT 1', function(err, _rows) {
+  if (err) throw err;
+
+  rows = _rows;
+});
+
+connection.end();
 
 process.on('exit', function() {
   assert.deepEqual(rows, [{1: 1}]);
