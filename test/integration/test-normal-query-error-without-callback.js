be71e38e119ad1e6c18b9bb16f70a9ede782912d
Felix Geisendörfer <felix@debuggable.com>
2012-05-06 13:32:50 +0200

    Fix remaining error handling cases

---
+++ test/integration/test-normal-query-error-without-callback.js
@@ -0,0 +1,19 @@
+var common     = require('../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+connection.connect();
+var query = connection.query('INVALID SQL');
+
+var err;
+query.on('error', function(_err) {
+  assert.equal(err, undefined);
+  err = _err;
+});
+
+connection.end();
+
+process.on('exit', function() {
+  assert.equal(err.code, 'ER_PARSE_ERROR');
+  assert.equal(Boolean(err.fatal), false);
+});
