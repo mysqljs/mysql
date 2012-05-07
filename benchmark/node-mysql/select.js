ed0fc1fb61620c3931bef25870966706b43a32f7
Felix Geisendörfer <felix@debuggable.com>
2012-05-07 11:30:16 +0200

    WIP

--- benchmark/node-mysql/select.js
+++ benchmark/node-mysql/select.js
@@ -1,25 +1,24 @@
 // Last v8 profile when running this test for 500k rows:
 // https://gist.github.com/f85c38010c038e5efe2e
-require('../../test/common');
-var Client = require('mysql/client'),
-    client = Client(TEST_CONFIG),
+var common = require('../../test/common');
+var Client = require('../../lib/client'),
+    client = common.createClient(),
     rows = 0;
 
 client.typeCast = false;
 
-client.connect();
-client.query('CREATE DATABASE '+TEST_DB, function(err) {
-  if (err && err.number != Client.ERROR_DB_CREATE_EXISTS) {
-    throw err;
-  }
+client.query('CREATE DATABASE '+common.TEST_DB, function(err) {
+  //if (err && err.number != Client.ERROR_DB_CREATE_EXISTS) {
+    //throw err;
+  //}
 });
 
-client.query('USE '+TEST_DB);
+client.query('USE '+common.TEST_DB);
 var selectStart = +new Date;
 
 function query() {
   client
-    .query('SELECT * FROM '+TEST_TABLE)
+    .query('SELECT * FROM '+common.TEST_TABLE)
     .on('row', function(row) {
       rows++;
     })
