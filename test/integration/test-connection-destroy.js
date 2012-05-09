567cbe6c7f9ec74284225007a5bd0c6c8ae8dd63
Felix Geisendörfer <felix@debuggable.com>
2012-05-09 11:56:14 +0200

    Error handling for queries after destroyed

---
+++ test/integration/test-connection-destroy.js
@@ -0,0 +1,9 @@
+var common     = require('../common');
+var connection = common.createConnection();
+var assert     = require('assert');
+
+connection.connect(function(err) {
+  if (err) throw err;
+
+  connection.destroy();
+});
