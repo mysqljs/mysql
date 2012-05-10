1c1c0c11c2eb168da8f4c49db22599b1683d6c56
Felix Geisendörfer <felix@debuggable.com>
2012-05-10 14:57:56 +0200

    Try to get tests to work on travis

--- test/common.js
+++ test/common.js
@@ -14,12 +14,20 @@ common.testDatabase = process.env.MYSQL_DATABASE;
 var Mysql = require('../');
 
 common.createConnection = function(config) {
-  return Mysql.createConnection(_.extend({
-    host     : process.env.MYSQL_HOST,
-    port     : process.env.MYSQL_PORT,
-    user     : process.env.MYSQL_USER,
-    password : process.env.MYSQL_PASSWORD,
-  }, config));
+  var isTravis = Boolean(process.env.CI);
+  if (isTravis) {
+    // see: http://about.travis-ci.org/docs/user/database-setup/
+    config = {user: 'root'};
+  } else {
+    config = _.extend({
+      host     : process.env.MYSQL_HOST,
+      port     : process.env.MYSQL_PORT,
+      user     : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD,
+    }, config)
+  }
+
+  return Mysql.createConnection(config);
 };
 
 common.useTestDb = function(connection) {
