18bd95cdae1a82e0c641d61c39347d0f46884571
Felix Geisendörfer <felix@debuggable.com>
2012-05-19 17:34:48 +0200

    Whitespace changes
    * Trailing whitespace
    * Missing whitespace between expressions
    * etc.

--- test/integration/test-change-user.js
+++ test/integration/test-change-user.js
@@ -1,22 +1,22 @@
 var common     = require('../common');
 var connection = common.createConnection();
 var assert     = require('assert');
+
 /**
 This test assumes there is a db named test on the MySQL server
 **/
-connection.query("select database() as current_db",function(err,info){
-  connection.changeUser({database:'test'},function(err){
-    connection.query("select database() as current_db",function(err,info){
-      if (err) {
-        throw err;
-      }
-      assert.equal(info[0]['current_db'],'test');
+
+connection.query("select database() as current_db", function(err, info){
+  connection.changeUser({database:'test'}, function(err){
+    connection.query("select database() as current_db", function(err, info){
+      if (err) throw err;
+
+      assert.equal(info[0]['current_db'], 'test');
       connection.end();
     });
   });
 });
-connection.on('error',function(err){
+
+connection.on('error', function(err){
   console.log(err);
 });
-
-
