00fbc94f1408fb262ca88488562bc52bb7dc10da
Felix Geisendörfer <felix@debuggable.com>
2012-05-23 12:53:55 +0200

    Long stack trace support for network errors

---
+++ test/integration/test-long-stack-traces-for-connection-errors.js
@@ -0,0 +1,12 @@
+var common     = require('../common');
+var connection = common.createConnection({port: common.bogusPort});
+var assert     = require('assert');
+
+var err;
+connection.connect(function(_err) {
+  err = _err;
+});
+
+process.on('exit', function() {
+  assert.ok(err.stack.indexOf(__filename) > 0);
+});
