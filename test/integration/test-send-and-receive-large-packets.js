4661cbaa5ab65cc74bb0eb9f57314d8e1a4157fa
Felix Geisendörfer <felix@debuggable.com>
2012-05-30 17:43:44 +0200

    Test: Adjust max_allowed_packet size if needed
    This should make everything pass on travis which seems to have a MySQL
    server configured for smaller max_allowed_packet values.

--- test/integration/test-send-and-receive-large-packets.js
+++ test/integration/test-send-and-receive-large-packets.js
@@ -2,26 +2,66 @@ var common     = require('../common');
 var connection = common.createConnection();
 var assert     = require('assert');
 
+connection.connect(function(err) {
+  if (err) throw err;
+
+  getMaxAllowedPacket();
+});
+
+
+var oldMaxAllowedPacket;
+function getMaxAllowedPacket() {
+  connection.query('SHOW VARIABLES WHERE Variable_name = ?', ['max_allowed_packet'], function(err, rows) {
+    if (err) throw err;
+
+    oldMaxAllowedPacket = Number(rows[0].Value);
+
+    increaseMaxAllowedPacketIfNeeded();
+  });
+}
+
+function increaseMaxAllowedPacketIfNeeded() {
+  // Our test generates a SQL query a few bytes larger than 16 MB, but lets
+  // leave a little margin:
+  var minMaxAllowedPacket = 20 * 1024 * 1024;
+
+  var newMaxAllowedPacket = (oldMaxAllowedPacket < minMaxAllowedPacket)
+    ? minMaxAllowedPacket
+    : oldMaxAllowedPacket;
+
+  connection.query('SET GLOBAL max_allowed_packet = ?', [newMaxAllowedPacket], function(err, rows) {
+    if (err) throw err;
+
+    triggerLargeQueryAndResponsePackets();
+  });
+}
+
+var rows = [];
 var length = Math.pow(256, 3) / 2; // Half, because of hex encoding
 var buffer = new Buffer(length);
 var sql    = 'SELECT ? as bigField';
 
-var rows = [];
-
-// Nesting the query inside the connect() method because our buffer to hex shim
-// for node v0.4.x takes ~12sec on TravisCI causing a handshake timeout unless
-// we do the handshake first before creating the SQL query.
-connection.connect(function(err) {
-  if (err) throw err;
-
+function triggerLargeQueryAndResponsePackets() {
   connection.query(sql, [buffer], function(err, _rows) {
     if (err) throw err;
 
     rows = _rows;
+
+    resetMaxAllowedPacket();
+  });
+}
+
+function resetMaxAllowedPacket() {
+  connection.query('SET GLOBAL max_allowed_packet = ?', [oldMaxAllowedPacket], function(err, rows) {
+    if (err) {
+      err.message = 'Could not reset max_allowed_packet size, please check your server settings: ' + err.message;
+      throw err;
+    }
   });
 
   connection.end();
-});
+}
+
 
 process.on('exit', function() {
   assert.equal(rows.length, 1);
