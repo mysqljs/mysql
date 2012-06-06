0e64d35690488d533c991fd3921539cb3d04d13f
Felix Geisendörfer <felix@debuggable.com>
2012-06-06 09:02:20 +0200

    Refactored type casting test
    More flexible, less magic now.

--- test/integration/test-type-casting.js
+++ test/integration/test-type-casting.js
@@ -4,63 +4,60 @@ var assert     = require('assert');
 
 common.useTestDb(connection);
 
-var expected = {
-  'DECIMAL'    : '0.330',
-  'TINYINT'    : 1,
-  'SMALLINT'   : 2,
-  'INT'        : 3,
-  'FLOAT'      : 4.5,
-  'DOUBLE'     : 5.5,
-  'BIGINT'     : '6',
-  'MEDIUMINT'  : 7,
-  'YEAR'       : 2012,
-  'TIMESTAMP'  : new Date('2012-05-12 11:00:23'),
-  'DATETIME'   : new Date('2012-05-12 11:02:32'),
-  'DATE'       : new Date('2012-05-12'),
-  'TIME'       : '13:13:23',
-  'BINARY'     : new Buffer([0, 1, 254, 255]),
-  'VARBINARY'  : new Buffer([0, 1, 254, 255]),
-  'TINYBLOB'   : new Buffer([0, 1, 254, 255]),
-  'MEDIUMBLOB' : new Buffer([0, 1, 254, 255]),
-  'LONGBLOB'   : new Buffer([0, 1, 254, 255]),
-  'BLOB'       : new Buffer([0, 1, 254, 255]),
-  'BIT'        : new Buffer([0, 1, 254, 255]),
-  'CHAR'       : 'Hello',
-  'VARCHAR'    : 'Hello',
-  'TINYTEXT'   : 'Hello World',
-  'MEDIUMTEXT' : 'Hello World',
-  'LONGTEXT'   : 'Hello World',
-  'TEXT'       : 'Hello World',
-};
+var tests = [
+  {type: 'decimal(3,3)', insert: '0.330'},
+  {type: 'decimal(3,3)', insert: 0.33, expect: '0.330'},
+  {type: 'tinyint', insert: 1},
+  {type: 'smallint', insert: 2},
+  {type: 'int', insert: 3},
+  {type: 'float', insert: 4.5},
+  {type: 'double', insert: 5.5},
+  {type: 'bigint', insert: '6'},
+  {type: 'bigint', insert: 6, expect: '6'},
+  {type: 'mediumint', insert: 7},
+  {type: 'year', insert: 2012},
+  {type: 'timestamp', insert: new Date('2012-05-12 11:00:23')},
+  {type: 'datetime', insert: new Date('2012-05-12 12:00:23')},
+  {type: 'date', insert: new Date('2012-05-12')},
+  {type: 'time', insert: '13:13:23'},
+  {type: 'binary(4)', insert: new Buffer([0, 1, 254, 255])},
+  {type: 'varbinary(4)', insert: new Buffer([0, 1, 254, 255])},
+  {type: 'tinyblob', insert: new Buffer([0, 1, 254, 255])},
+  {type: 'mediumblob', insert: new Buffer([0, 1, 254, 255])},
+  {type: 'longblob', insert: new Buffer([0, 1, 254, 255])},
+  {type: 'blob', insert: new Buffer([0, 1, 254, 255])},
+  {type: 'bit(32)', insert: new Buffer([0, 1, 254, 255])},
+  {type: 'char(5)', insert: 'Hello'},
+  {type: 'varchar(5)', insert: 'Hello'},
+  {type: 'tinytext', insert: 'Hello World'},
+  {type: 'mediumtext', insert: 'Hello World'},
+  {type: 'longtext', insert: 'Hello World'},
+  {type: 'text', insert: 'Hello World'},
+];
+
+var table = 'type_casting';
 
 var schema  = [];
 var inserts = [];
-for (var key in expected) {
-  var value = expected[key];
-  var type = key;
-  if (type === 'DECIMAL') {
-    type = type + '(3,3)';
-  } else if (/binary|char/i.test(type)) {
-    type = type + '(' + value.length + ')';
-  } else if (/bit/i.test(type)) {
-    type = type + '(' + (value.length * 8) + ')';
-  }
 
-  var escaped = connection.escape(value);
+tests.forEach(function(test, index) {
+  var escaped = connection.escape(test.insert);
 
-  schema.push('`' + key + '` ' + type + ',');
-  inserts.push('`' + key + '` = ' + escaped);
-}
+  schema.push('`' + index + '` ' + test.type + ',');
+  inserts.push('`' + index + '` = ' + escaped);
+});
 
-connection.query([
-  'CREATE TEMPORARY TABLE `type_casting` (',
+var createTable = [
+  'CREATE TEMPORARY TABLE `' + table + '` (',
   '`id` int(11) unsigned NOT NULL AUTO_INCREMENT,',
   ].concat(schema).concat([
   'PRIMARY KEY (`id`)',
   ') ENGINE=InnoDB DEFAULT CHARSET=utf8'
-]).join('\n'));
+]).join('\n');
 
-connection.query('INSERT INTO type_casting SET' + inserts.join(',\n'));
+connection.query(createTable);
+
+connection.query('INSERT INTO ' + table + ' SET' + inserts.join(',\n'));
 
 var row;
 connection.query('SELECT * FROM type_casting', function(err, rows) {
@@ -72,18 +69,23 @@ connection.query('SELECT * FROM type_casting', function(err, rows) {
 connection.end();
 
 process.on('exit', function() {
-  for (var key in expected) {
-    var expectedValue = expected[key];
-    var actualValue   = row[key];
+  tests.forEach(function(test, index) {
+    var expected = test.expect || test.insert;
+    var got      = row[index];
 
-    if (expectedValue instanceof Date) {
-      expectedValue = Number(expectedValue);
-      actualValue   = Number(actualValue);
-    } else if (Buffer.isBuffer(expectedValue)) {
-      expectedValue = Array.prototype.slice.call(expectedValue)+'';
-      actualValue   = Array.prototype.slice.call(actualValue)+'';
+    if (expected instanceof Date) {
+      assert.equal(got instanceof Date, true);
+
+      expected = String(expected);
+      got      = String(got);
+    } else if (Buffer.isBuffer(expected)) {
+      assert.equal(Buffer.isBuffer(got), true);
+
+      expected = String(Array.prototype.slice.call(expected));
+      got      = String(Array.prototype.slice.call(got));
     }
 
-    assert.strictEqual(actualValue, expectedValue, key + ': ' + actualValue + ' !== ' + expectedValue);
-  }
+    var message = 'got: ' + got + ' expected: ' + expected + ' ' + test.type;
+    assert.strictEqual(expected, got, message);
+  });
 });
