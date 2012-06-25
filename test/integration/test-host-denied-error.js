f57f161d1b301a24655751a4d0ef4cfb75d3249c
Felix Geisendörfer <felix@debuggable.com>
2012-06-25 05:56:40 +0200

    Properly parse host denied / similar errors
    They use the old 4.0 format (no sqlStateMarker / sqlState).

--- test/integration/test-host-denied-error.js
+++ test/integration/test-host-denied-error.js
@@ -21,5 +21,6 @@ server.on('connection', function(incomingConnection) {
 
 process.on('exit', function() {
   assert.equal(connectErr.code, 'ER_HOST_NOT_PRIVILEGED');
+  assert.ok(/You suck/.test(connectErr.message));
   assert.equal(connectErr.fatal, true);
 });
