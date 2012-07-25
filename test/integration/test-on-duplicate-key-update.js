a013b96420a140c977206cf64dcc217a09bb7f16
Iskren Chernev <iskren@tradeo.com>
2012-07-25 15:57:30 +0300

    Improved assert messages in test-on-duplicate-key-update

--- test/integration/test-on-duplicate-key-update.js
+++ test/integration/test-on-duplicate-key-update.js
@@ -19,17 +19,17 @@ connection.query('INSERT INTO `' + table + '` SET ?', {a: 1, b: 1, c: 1});
 
 connection.query('INSERT INTO `' + table + '` (a, b, c) VALUES (1, 2, 3) ON DUPLICATE KEY UPDATE c = 1', function(err, info) {
   assert.strictEqual(null, err);
-  assert.strictEqual(0, info.affectedRows, 'both primary key and updated key are the same so nothing is affected');
+  assert.strictEqual(0, info.affectedRows, 'both primary key and updated key are the same so nothing is affected (expected 0, got ' + info.affectedRows + ' affectedRows)');
 });
 
 connection.query('INSERT INTO `' + table + '` (a, b, c) VALUES (2, 3, 4) ON DUPLICATE KEY UPDATE c = 1', function(err, info) {
   assert.strictEqual(null, err);
-  assert.strictEqual(1, info.affectedRows, 'primary key differs, so new row is inserted');
+  assert.strictEqual(1, info.affectedRows, 'primary key differs, so new row is inserted (expected 1, got ' + info.affectedRows + ' affectedRows)');
 });
 
 connection.query('INSERT INTO `' + table + '` (a, b, c) VALUES (1, 2, 3) ON DUPLICATE KEY UPDATE c = 2', function(err, info) {
   assert.strictEqual(null, err);
-  assert.strictEqual(2, info.affectedRows, 'primary key is the same, row is updated');
+  assert.strictEqual(2, info.affectedRows, 'primary key is the same, row is updated (expected 2, got ' + info.affectedRows + ' affectedRows)');
 });
 
 connection.end()
