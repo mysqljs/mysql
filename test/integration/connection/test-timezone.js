4d95a9fb4fd8f5cb44acd9ad22b680d28a549c4c
Bryan Donovan <bdondo@gmail.com>
2012-09-12 15:00:18 -0700

    working utc config option

---
+++ test/integration/connection/test-timezone.js
@@ -0,0 +1,40 @@
+//process.env.TZ = 'UTC';
+var common = require('../../common');
+var assert = require('assert');
+
+var config = {utc: true};
+var connection = common.createConnection(config);
+
+common.useTestDb(connection);
+
+var table = 'insert_test';
+connection.query([
+  'CREATE TEMPORARY TABLE `' + table + '` (',
+  '`id` int(11) unsigned NOT NULL AUTO_INCREMENT,',
+  '`created_at` datetime,',
+  'PRIMARY KEY (`id`)',
+  ') ENGINE=InnoDB DEFAULT CHARSET=utf8'
+].join('\n'));
+
+var result;
+var now = new Date();
+var nowString = now.toISOString();
+
+connection.query('INSERT INTO ' + table + ' SET ?', {created_at: nowString}, function(err, _result) {
+  if (err) throw err;
+
+  var sql = 'SELECT * FROM ' + table + ' WHERE id = ?';
+  connection.query(sql, [_result.insertId], function(err, _result) {
+    connection.end();
+    if (err) throw err;
+
+    result = _result[0].created_at;
+  });
+});
+
+
+process.on('exit', function() {
+  var expectedHours = now.getHours();
+  var actualHours = result.getHours();
+  assert.strictEqual(actualHours, expectedHours);
+});
