492021b70101aba62ed39aa7e4a5b84ebfa78463
Diogo Resende <dresende@thinkdigital.pt>
2012-10-31 10:55:27 +0000

    Export mysql column types constants
    This will come in handy later when using a custom typeCast. We could
    also create some alias, because some constants don't have the same name
    as people are used to (e.g. LONGLONG -> BIGINT, TINY -> TINYINT)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var Connection       = require('./lib/Connection');
 var ConnectionConfig = require('./lib/ConnectionConfig');
+var Types            = require('./lib/protocol/constants/types');
 
 exports.createConnection = function(config) {
   return new Connection({config: new ConnectionConfig(config)});
 };
 
 exports.escape = require('./lib/protocol/SqlString').escape;
+exports.Types = Types;
