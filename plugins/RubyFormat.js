72b8f29ad8f575d9a52d0535eac4de4e00c5df5d
Diogo Resende <dresende@thinkdigital.pt>
2012-11-02 22:31:07 +0000

    Updates RubyFormat plugin to use SqlString to escape values

--- plugins/RubyFormat.js
+++ plugins/RubyFormat.js
@@ -1,9 +1,14 @@
+var SqlString = require('../lib/protocol/SqlString');
+
 module.exports = function (Connection) {
   Connection.format = function (sql, values) {
     values = values || {};
 
     return sql.replace(/\:(\w+)/, function (m, k) {
-      return values[k] || 'NULL';
+      if (!values.hasOwnProperty(k)) {
+        return 'NULL';
+      }
+      return SqlString.escape(values[k], false, Connection._timezone);
     });
   };
 };
