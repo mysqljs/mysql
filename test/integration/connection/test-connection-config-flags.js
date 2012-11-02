7e224fb028d045fc45224abcec3b470294ea9f05
Diogo Resende <dresende@thinkdigital.pt>
2012-11-02 20:24:22 +0000

    Pass flag parsing (split) to ConnectionConfig.mergeFlags
    This way, tests can also check for correct flag string splitting

--- test/integration/connection/test-connection-config-flags.js
+++ test/integration/connection/test-connection-config-flags.js
@@ -4,41 +4,45 @@ var assert           = require('assert');
 
 var testFlags = [{
   'default' : [ '' ],
-  'user'    : [ 'LONG_PASSWORD' ],
+  'user'    : 'LONG_PASSWORD',
   'expected': ClientConstants.CLIENT_LONG_PASSWORD
 }, {
   'default' : [ '' ],
-  'user'    : [ '-LONG_PASSWORD' ],
+  'user'    : '-LONG_PASSWORD',
   'expected': 0x0
 }, {
   'default' : [ 'LONG_PASSWORD', 'FOUND_ROWS' ],
-  'user'    : [ '-LONG_PASSWORD' ],
+  'user'    : '-LONG_PASSWORD',
   'expected': ClientConstants.CLIENT_FOUND_ROWS
 }, {
   'default' : [ 'LONG_PASSWORD', 'FOUND_ROWS' ],
-  'user'    : [ '-FOUND_ROWS' ],
+  'user'    : '-FOUND_ROWS',
   'expected': ClientConstants.CLIENT_LONG_PASSWORD
 }, {
   'default' : [ 'LONG_PASSWORD', 'FOUND_ROWS' ],
-  'user'    : [ '-LONG_FLAG' ],
+  'user'    : '-LONG_FLAG',
   'expected': ClientConstants.CLIENT_LONG_PASSWORD |
               ClientConstants.CLIENT_FOUND_ROWS
 }, {
   'default' : [ 'LONG_PASSWORD', 'FOUND_ROWS' ],
-  'user'    : [ 'LONG_FLAG' ],
+  'user'    : 'LONG_FLAG',
   'expected': ClientConstants.CLIENT_LONG_PASSWORD |
               ClientConstants.CLIENT_FOUND_ROWS |
               ClientConstants.CLIENT_LONG_FLAG
 }, {
   'default' : [ 'LONG_PASSWORD', 'FOUND_ROWS' ],
-  'user'    : [ 'UNDEFINED_CONSTANT' ],
+  'user'    : 'UNDEFINED_CONSTANT',
   'expected': ClientConstants.CLIENT_LONG_PASSWORD |
               ClientConstants.CLIENT_FOUND_ROWS
 }, {
   'default' : [ 'LONG_PASSWORD', 'FOUND_ROWS' ],
-  'user'    : [ '-UNDEFINED_CONSTANT' ],
+  'user'    : '-UNDEFINED_CONSTANT',
   'expected': ClientConstants.CLIENT_LONG_PASSWORD |
               ClientConstants.CLIENT_FOUND_ROWS
+}, {
+  'default' : [ 'LONG_PASSWORD', 'FOUND_ROWS' ],
+  'user'    : '-UNDEFINED_CONSTANT,,  -found_ROWS',
+  'expected': ClientConstants.CLIENT_LONG_PASSWORD
 }];
 
 for (var i = 0; i < testFlags.length; i++) {
