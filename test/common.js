08591a66bb969b37862a4bb0566e2cea92a31633
Diogo Resende <dresende@thinkdigital.pt>
2012-12-10 15:58:57 +0000

    Extends config from env variables like createConnection

--- test/common.js
+++ test/common.js
@@ -43,7 +43,19 @@ common.createConnection = function(config) {
 };
 
 common.createPool = function(config) {
-  config = config || {};
+  if (common.isTravis()) {
+    // see: http://about.travis-ci.org/docs/user/database-setup/
+    config = _.extend({
+      user: 'root'
+    }, config);
+  } else {
+    config = _.extend({
+      host     : process.env.MYSQL_HOST,
+      port     : process.env.MYSQL_PORT,
+      user     : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD
+    }, config);
+  }
   config.createConnection = common.createConnection;
   return Mysql.createPool(config);
 };
