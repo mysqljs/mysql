94629125050c06167c4466185853146e36a3f671
Stephen Sugden <glurgle@gmail.com>
2012-12-13 00:26:18 -0800

    export Query constructor

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var Connection       = require('./lib/Connection');
 var ConnectionConfig = require('./lib/ConnectionConfig');
 var Types            = require('./lib/protocol/constants/types');
 var SqlString        = require('./lib/protocol/SqlString');
+var Query            = require('./lib/protocol/sequences/Query');
 
 exports.createConnection = function(config) {
   return new Connection({config: new ConnectionConfig(config)});
 };
 
 exports.Types    = Types;
+exports.Query    = Query;
 exports.escape   = SqlString.escape;
 exports.escapeId = SqlString.escapeId;
