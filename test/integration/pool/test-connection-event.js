fe4a745109362a6cb8c6e6782c7651135c244dc6
Marcus Westin <marcus.westin@gmail.com>
2013-05-13 19:26:41 -0700

    Add Pool `connection` event, which fires when the Pool has created a new connection. This allows for e.g. setting Session variables on the connection before it gets used

---
+++ test/integration/pool/test-connection-event.js
@@ -0,0 +1,15 @@
+var common     = require('../../common');
+var assert     = require('assert');
+var Connection = require(common.lib + '/Connection');
+var pool       = common.createPool();
+
+var connectionEventHappened = false;
+pool.on('connection', function(err, connection) {
+  connectionEventHappened = true;
+})
+
+pool.getConnection(function(err, connection) {
+  if (err) throw err;
+  assert.equal(connectionEventHappened, true);
+  pool.end();
+});
