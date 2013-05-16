b525950c116b51ca6749d44eb2e8ce76d18d4661
Kai Koch <kai.koch@gmail.com>
2013-05-17 00:00:35 +0300

    test-unix-domain-socket.js: test reverted to previous version, added if-clause in the beginning
    Reverted the test to the previous behaviour and added an IF-Clause in the beginning that skips the test if ```windir``` is set.

--- test/integration/connection/test-unix-domain-socket.js
+++ test/integration/connection/test-unix-domain-socket.js
@@ -1,21 +1,23 @@
 /**
- * If on a windows system, there are no unix sockets.
- * So we skip this test and defaulting the asserts in the
- * process.on('exit', [...]) function to false, when we find the
- * enviroment variable 'windir' is set. I assume 'windir' will not be set on
- * *nix-systems.
+ * This test is skipped, if the environment variable "windir" is set.
+ * It assumes that it runs on a windows system then.
  */
+if (process.env.windir) {
+  return console.log('Skipping "test-unix-domain-socket.js" - Environment' 
+    + ' variable "windir" is set. Skipping this, because we seem to be on' 
+    + ' a windows system');  
+}
 var common     = require('../../common');
 var connection = common.createConnection({socketPath: common.fakeServerSocket});
 var assert     = require('assert');
 
 var server = common.createFakeServer();
 var didConnect = false;
-if (!process.env.windir) {
-  server.listen(common.fakeServerSocket, function(err) {
-    if (err) throw err;
 
-    connection.connect(function(err) {
+server.listen(common.fakeServerSocket, function(err) {
+  if (err) throw err;
+
+  connection.connect(function(err) {
     if (err) throw err;
 
     assert.equal(didConnect, false);
@@ -23,26 +25,18 @@ if (!process.env.windir) {
 
     connection.destroy();
     server.destroy();
-    });
   });
-}
+});
 
 var hadConnection = false;
-if (!process.env.windir) {
-  server.on('connection', function(connection) {
-    connection.handshake();
+server.on('connection', function(connection) {
+  connection.handshake();
 
-    assert.equal(hadConnection, false);
-    hadConnection = true;
-  });
-}
+  assert.equal(hadConnection, false);
+  hadConnection = true;
+});
 
 process.on('exit', function() {
-  if (process.env.windir) {
-    assert.equal(didConnect, false);
-    assert.equal(hadConnection, false);
-  } else {
     assert.equal(didConnect, true);
     assert.equal(hadConnection, true);
-  }
 });
