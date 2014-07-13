87f1078c72a18c1c957bada8e5af6a9b3d48daac
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-07-13 00:38:25 -0400

    Make errors from the parser catchable

---
+++ test/unit/connection/test-parser-error.js
@@ -0,0 +1,40 @@
+var assert       = require('assert');
+var common       = require('../../common');
+var connection   = common.createConnection({port: common.fakeServerPort});
+var Packets      = require(common.lib + '/protocol/packets');
+var PacketWriter = require(common.lib + '/protocol/PacketWriter');
+var server       = common.createFakeServer();
+
+server.listen(common.fakeServerPort, function(err) {
+  assert.ifError(err);
+
+  connection.query('SELECT value FROM stuff', function (err) {
+    assert.ok(err, 'got err');
+    assert.equal(err.code, 'PARSER_READ_PAST_END');
+    assert.equal(!!err.fatal, false);
+    assert.equal(err.offset, 4);
+    connection.destroy();
+    server.destroy();
+  });
+});
+
+server.on('connection', function(conn) {
+  conn.handshake();
+  conn.on('query', function(packet) {
+    switch (packet.sql) {
+      case 'SELECT value FROM stuff':
+        this._sendPacket(new Packets.ResultSetHeaderPacket({
+          fieldCount: 1
+        }));
+
+        var writer = new PacketWriter();
+        writer.writeLengthCodedString('def');
+        this._socket.write(writer.toBuffer(this._parser));
+        this._parser.resetPacketNumber();
+        break;
+      default:
+        this._handlePacketQuery(packet);
+        break;
+    }
+  });
+});
