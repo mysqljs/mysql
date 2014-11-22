83a0cc20790645f04794482420a721aa34603a4f
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-11-21 20:53:38 -0500

    tests: add PoolConnection.pause/resume tests
    closes #947

---
+++ test/unit/pool/test-query-streaming-rows.js
@@ -0,0 +1,51 @@
+var assert = require('assert');
+var common = require('../../common');
+var pool   = common.createPool({port: common.fakeServerPort});
+
+var server = common.createFakeServer();
+
+server.listen(common.fakeServerPort, function (err) {
+  assert.ifError(err);
+
+  var wait = 2;
+  function done() {
+    if (--wait) return;
+    pool.end(function (err) {
+      assert.ifError(err);
+      server.destroy();
+    });
+  }
+
+  pool.getConnection(function (err, connection) {
+    assert.ifError(err);
+
+    var count  = 0;
+    var paused = false;
+    var query  = connection.query('SELECT * FROM stream LIMIT 5');
+
+    query.on('fields', function (fields) {
+      assert.ok(fields, 'got fields');
+      done();
+    });
+
+    query.on('result', function (row) {
+      count++;
+
+      assert.equal(paused, false);
+      assert.equal(row.id, count);
+
+      paused = true;
+      connection.pause();
+
+      setTimeout(function () {
+        paused = false;
+        connection.resume();
+      }, 50);
+    });
+
+    query.on('end', function () {
+      connection.release();
+      done();
+    });
+  });
+});
