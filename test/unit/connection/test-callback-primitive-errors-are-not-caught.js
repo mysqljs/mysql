cfd0ce3572d75c3c82103418d1d03cbe67eaf8a1
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-12-16 20:35:05 -0500

    Fix error if falsy error thrown in callback handler
    fixes #960

---
+++ test/unit/connection/test-callback-primitive-errors-are-not-caught.js
@@ -0,0 +1,29 @@
+var assert     = require('assert');
+var common     = require('../../common');
+var connection = common.createConnection({port: common.fakeServerPort});
+
+var error  = undefined;
+var server = common.createFakeServer();
+
+server.listen(common.fakeServerPort, function (err) {
+  assert.ifError(err);
+
+  var caughtErr = null;
+
+  connection.connect(function (err) {
+    assert.ifError(err);
+
+    process.once('uncaughtException', function (err) {
+      caughtErr = err;
+    });
+
+    throw error;
+  });
+
+  connection.end(function (err) {
+    process.removeAllListeners('uncaughtException');
+    assert.ifError(err);
+    assert.strictEqual(caughtErr, error);
+    server.destroy();
+  });
+});
