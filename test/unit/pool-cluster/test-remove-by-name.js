7bacf3b20553810701baf75d41e941558de57680
Malyshev Dmitry <softic1@yandex.ru>
2015-02-21 17:47:47 +0300

    Add poolCluster.remove to remove pools from the cluster
    closes #1006
    closes #1007

---
+++ test/unit/pool-cluster/test-remove-by-name.js
@@ -0,0 +1,40 @@
+var assert  = require('assert');
+var common  = require('../../common');
+var cluster = common.createPoolCluster();
+var server  = common.createFakeServer();
+
+var poolConfig = common.getTestConfig({port: common.fakeServerPort});
+cluster.add('SLAVE1', poolConfig);
+cluster.add('SLAVE2', poolConfig);
+
+server.listen(common.fakeServerPort, function (err) {
+  assert.ifError(err);
+
+  var pool = cluster.of('SLAVE*', 'ORDER');
+
+  pool.getConnection(function (err, connection) {
+    assert.ifError(err);
+    assert.strictEqual(connection._clusterId, 'SLAVE1');
+
+    connection.release();
+    cluster.remove('SLAVE1');
+
+    pool.getConnection(function (err, connection) {
+      assert.ifError(err);
+      assert.strictEqual(connection._clusterId, 'SLAVE2');
+
+      connection.release();
+      cluster.remove('SLAVE2');
+
+      pool.getConnection(function (err, connection) {
+        assert.ok(err);
+        assert.equal(err.code, 'POOL_NOEXIST');
+
+        cluster.remove('SLAVE1');
+        cluster.remove('SLAVE2');
+
+        server._server.close();
+      });
+    });
+  });
+});
---
+++ test/unit/pool-cluster/test-remove-by-pattern.js
@@ -0,0 +1,31 @@
+var assert  = require('assert');
+var common  = require('../../common');
+var cluster = common.createPoolCluster();
+var server  = common.createFakeServer();
+
+var poolConfig = common.getTestConfig({port: common.fakeServerPort});
+cluster.add('SLAVE1', poolConfig);
+cluster.add('SLAVE2', poolConfig);
+
+server.listen(common.fakeServerPort, function (err) {
+  assert.ifError(err);
+
+  var pool = cluster.of('SLAVE*', 'ORDER');
+
+  pool.getConnection(function (err, connection) {
+    assert.ifError(err);
+    assert.strictEqual(connection._clusterId, 'SLAVE1');
+
+    connection.release();
+    cluster.remove('SLAVE*');
+
+    pool.getConnection(function (err, connection) {
+      assert.ok(err);
+      assert.equal(err.code, 'POOL_NOEXIST');
+
+      cluster.remove('SLAVE*');
+
+      server._server.close();
+    });
+  });
+});
