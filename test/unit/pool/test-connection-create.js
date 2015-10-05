f5b1b6ab923bfffcb9c18f58658c44932923643b
Alex Corbi <a.corbi@gmail.com>
2015-10-05 16:24:02 +0200

    Added unity test for createConnection method

---
+++ test/unit/pool/test-connection-create.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var common = require('../../common');
+var Connection = common.Connection;
+var EventEmitter = require('events').EventEmitter;
+var pool = common.createPool({
+  port: common.fakeServerPort
+});
+var PoolConnection = common.PoolConnection;
+
+var server = common.createFakeServer();
+
+server.listen(common.fakeServerPort, function(err) {
+  assert.ifError(err);
+
+  pool.createConnection(function(err, connection) {
+    assert.ifError(err);
+
+    assert(connection instanceof PoolConnection);
+    assert(connection instanceof Connection);
+    assert(connection instanceof EventEmitter);
+
+    connection.destroy();
+    server.destroy();
+  });
+});
