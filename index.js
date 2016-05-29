5a65171b45fef19c0aca49c8d05278cb3f92a949
Douglas Christopher Wilson <doug@somethingdoug.com>
2016-05-28 20:28:57 -0400

    lint: require valid JSDoc comments

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var Classes = Object.create(null);
 
 /**
  * Create a new Connection instance.
- * @param {object} config
+ * @param {object|string} config Configuration or connection string for new MySQL connection
+ * @return {Connection} A new MySQL connection
  * @public
  */
 exports.createConnection = function createConnection(config) {
@@ -14,7 +15,8 @@ exports.createConnection = function createConnection(config) {
 
 /**
  * Create a new Pool instance.
- * @param {object} config
+ * @param {object|string} config Configuration or connection string for new MySQL connections
+ * @return {Pool} A new MySQL pool
  * @public
  */
 exports.createPool = function createPool(config) {
@@ -26,7 +28,8 @@ exports.createPool = function createPool(config) {
 
 /**
  * Create a new PoolCluster instance.
- * @param {object} config
+ * @param {object} [config] Configuration for pool cluster
+ * @return {PoolCluster} New MySQL pool cluster
  * @public
  */
 exports.createPoolCluster = function createPoolCluster(config) {
@@ -37,6 +40,10 @@ exports.createPoolCluster = function createPoolCluster(config) {
 
 /**
  * Create a new Query instance.
+ * @param {string} sql The SQL for the query
+ * @param {array} [values] Any values to insert into placeholders in sql
+ * @param {function} [callback] The callback to use when query is complete
+ * @return {Query} New query object
  * @public
  */
 exports.createQuery = function createQuery(sql, values, callback) {
@@ -47,9 +54,10 @@ exports.createQuery = function createQuery(sql, values, callback) {
 
 /**
  * Escape a value for SQL.
- * @param {*} value
- * @param {boolean} [stringifyObjects=false]
- * @param {string} [timeZone=local]
+ * @param {*} value The value to escape
+ * @param {boolean} [stringifyObjects=false] Setting if objects should be stringified
+ * @param {string} [timeZone=local] Setting for time zone to use for Date conversion
+ * @return {string} Escaped string value
  * @public
  */
 exports.escape = function escape(value, stringifyObjects, timeZone) {
@@ -60,8 +68,9 @@ exports.escape = function escape(value, stringifyObjects, timeZone) {
 
 /**
  * Escape an identifier for SQL.
- * @param {*} value
- * @param {boolean} [forbidQualified]
+ * @param {*} value The value to escape
+ * @param {boolean} [forbidQualified=false] Setting to treat '.' as part of identifier
+ * @return {string} Escaped string value
  * @public
  */
 exports.escapeId = function escapeId(value, forbidQualified) {
@@ -72,10 +81,11 @@ exports.escapeId = function escapeId(value, forbidQualified) {
 
 /**
  * Format SQL and replacement values into a SQL string.
- * @param {string} sql
- * @param {array} [values]
- * @param {boolean} [stringifyObjects=false]
- * @param {string} [timeZone=local]
+ * @param {string} sql The SQL for the query
+ * @param {array} [values] Any values to insert into placeholders in sql
+ * @param {boolean} [stringifyObjects=false] Setting if objects should be stringified
+ * @param {string} [timeZone=local] Setting for time zone to use for Date conversion
+ * @return {string} Formatted SQL string
  * @public
  */
 exports.format = function format(sql, values, stringifyObjects, timeZone) {
@@ -94,6 +104,8 @@ Object.defineProperty(exports, 'Types', {
 
 /**
  * Load the given class.
+ * @param {string} className Name of class to default
+ * @return {function|object} Class constructor or exports
  * @private
  */
 function loadClass(className) {
