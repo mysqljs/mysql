076ad07701f043913803f9a91fc5729de99b3afd
alsotang <alsotang@gmail.com>
2016-09-27 12:11:38 +0800

    more tests

---
+++ test/unit/pool-cluster/test-query-pool-error.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var common = require('../../common');
+
+var cluster = common.createPoolCluster();
+var server  = common.createFakeServer();
+
+var poolConfig = common.getTestConfig({port: common.fakeServerPort});
+// cluster.add('SLAVE1', poolConfig);
+// cluster.add('SLAVE2', poolConfig);
+
+server.listen(common.fakeServerPort, function (err) {
+  assert.ifError(err);
+
+  var pool = cluster.of('SLAVE*', 'ORDER');
+
+  pool.query('SELECT 1', function (err, rows) {
+    assert.ok(err, 'got error');
+    assert.equal(err.code, 'POOL_NOEXIST');
+    server.destroy();
+  });
+});
