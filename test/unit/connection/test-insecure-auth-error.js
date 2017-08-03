9db93eb2529d6f64654aed1e803feb823eeee713
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-08-03 10:58:44 -0400

    tests: add test for insecure auth error

---
+++ test/unit/connection/test-insecure-auth-error.js
@@ -0,0 +1,29 @@
+var assert     = require('assert');
+var common     = require('../../common');
+var connection = common.createConnection({
+  port     : common.fakeServerPort,
+  password : 'oldpw'
+});
+
+var server = common.createFakeServer();
+
+server.listen(common.fakeServerPort, function (err) {
+  assert.ifError(err);
+
+  connection.connect(function (err) {
+    assert.ok(err);
+    assert.equal(err.code, 'HANDSHAKE_INSECURE_AUTH');
+    assert.equal(err.fatal, true);
+
+    connection.destroy();
+    server.destroy();
+  });
+});
+
+server.on('connection', function(incomingConnection) {
+  incomingConnection.handshake({
+    user        : connection.config.user,
+    password    : connection.config.password,
+    oldPassword : true
+  });
+});
